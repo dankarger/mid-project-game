aa05200e57df924404b36a850456c2155cf0d657
dankarger <karger.dan@gmail.com>
2022-01-02 01:57:37 +0200

    add selected div create page 2

--- src/pages/CreatePlayerPage/CreatePlayerPage.jsx
+++ src/pages/CreatePlayerPage/CreatePlayerPage.jsx
@@ -84,17 +84,17 @@ const CreatePlayerPage=({callback})=>{
              <div className='avatars-div'>
                  {/*TODO:make a avatar-img-div component*/}
                  <div className={selectedAvatar==='ALONZO'?'avatar-img-div active':'avatar-img-div '} onClick={()=>handleSelectedAvatarDiv('ALONZO')} id='div1'>
-                     <input type="radio" id="avatar1" name="drone" value="ALONZO" onChange={handleRadioInputOnChange} defaultChecked={true}/>
+                     {/*<input type="radio" id="avatar1" name="drone" value="ALONZO" onChange={handleRadioInputOnChange} defaultChecked={true}/>*/}
                          <label htmlFor="huey">ALONZO</label>
                          <img src={AVATARS['ALONZO']} alt="avatar1"/>
                  </div>
                  <div className={selectedAvatar==='The_KING'?'avatar-img-div active':'avatar-img-div '} onClick={()=>handleSelectedAvatarDiv('The_KING')}>
-                     <input type="radio" id="avatar2" name="drone" value="The_KING" onChange={handleRadioInputOnChange}/>
+                     {/*<input type="radio" id="avatar2" name="drone" value="The_KING" onChange={handleRadioInputOnChange}/>*/}
                          <label htmlFor="The KING">The KING</label>
                      <img src={AVATARS['The_KING']} alt="avatar2"/>
                  </div>
                  <div className={selectedAvatar==='LOUIE'?'avatar-img-div active':'avatar-img-div '} onClick={()=>handleSelectedAvatarDiv('LOUIE')}>
-                     <input type="radio" id="avatar3" name="drone" value="LOUIE" onChange={handleRadioInputOnChange}/>
+                     {/*<input type="radio" id="avatar3" name="drone" value="LOUIE" onChange={handleRadioInputOnChange}/>*/}
                          <label htmlFor="LOUIE">LOUIE</label>
                      <img src={AVATARS['LOUIE']} alt="avatar3"/>
                  </div>
