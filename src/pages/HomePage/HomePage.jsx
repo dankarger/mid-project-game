7a93c96822f4cac1a34d3ceea2e89768aea3d15e
dankarger <karger.dan@gmail.com>
2022-01-02 16:55:34 +0200

    removing icons from home page buttons

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -17,9 +17,9 @@ const HomePage = () => {
         <div className='home-content'>
             <h1>Title</h1>
            <div className='home-menu-divs'>
-              <Button callback={handleNewGameButton} className='home-page'  icon='s' name='New Game'/>
-               <Link to='/settings' > <Button className='home-page'  icon='s' name='Settings'/> </Link>
-               <Link to='/game' > <Button className='home-page'  icon='s' name='Quit'/> </Link>
+              <Button callback={handleNewGameButton} className='home-page'   name='New Game'/>
+               <Link to='/settings' > <Button className='home-page'   name='Settings'/> </Link>
+               <Link to='/game' > <Button className='home-page'   name='Quit'/> </Link>
            </div>
             <div className={isCreatePlayer?'show':'hide'}>
                 <CreatePlayerPage callback={()=>setIsCreatePlayer(!setIsCreatePlayer)}/>
