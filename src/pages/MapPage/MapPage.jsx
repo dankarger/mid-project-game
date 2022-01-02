32b61fb6a64d3ccbbff05cde1c6c7d7e57415fc8
dankarger <karger.dan@gmail.com>
2022-01-03 01:00:33 +0200

    add spring animation to mapPage

--- src/pages/MapPage/MapPage.jsx
+++ src/pages/MapPage/MapPage.jsx
@@ -2,7 +2,7 @@ import React, {useContext, useEffect, useState} from "react";
 import './MapPage.css'
 import Button from "../../componenets/utility/Button/Button";
 import {GameDataContext} from "../../componenets/GameLogic/GameLogic";
-
+import {useSpring, animated, config} from "react-spring";
 
 const MapPage =({callback})=> {
     const currentRoomData = useContext(GameDataContext);
@@ -13,28 +13,32 @@ const MapPage =({callback})=> {
     },[currentRoomData.value])
 
 
-    console.log(activeRoom)
+    const styles = useSpring({
+        loop:true,
+        config:{config:config.slow},
+        to:{scale:0.8},
+        from:{scale:1.1}
+    })
 
     return (
         <div className='map'>
         <h1>Current Level:{currentRoomData.value}</h1>
             <div className="rooms-div">
-                <div className="row">
+                <animated.div style={activeRoom===5?styles:{}} className="row">
                     <Button disabled={activeRoom!==5} className={activeRoom>5?'map-button button5 finished':'map-button button5'} callback={()=>{callback('boss')}} value='5' name='Boss' />
-                </div>
-                <div className="row">
+                </animated.div>
+                <animated.div style={activeRoom===4?styles:{}} className="row">
                     <Button disabled={activeRoom!==4} className={activeRoom>4?'map-button button4 finished':'map-button button4'} callback={()=>{callback('room4')}} value='4' name='Level 4' />
-                </div>
-                <div className="row">
-                    <Button disabled={activeRoom!==3} className={activeRoom>3?'map-button button3 finished':'map-button button3'} callback={()=>{callback('room3')}} value='3' name='Level 3' />
-                </div>
-                <div className="row">
-
+                </animated.div>
+                <animated.div style={activeRoom===3?styles:{}} className="row">
+                    <Button  disabled={activeRoom!==3} className={activeRoom>3?'map-button button3 finished':'map-button button3'} callback={()=>{callback('room3')}} value='3' name='Level 3' />
+                </animated.div>
+                <animated.div style={activeRoom===2?styles:{}} className="row">
                     <Button disabled={activeRoom!==2} className={activeRoom>2?'map-button button2 finished':'map-button button2'} callback={()=>{callback('room2')}} value='2' name='Level 2' />
-                </div>
-                <div className="row">
+                </animated.div>
+                <animated.div style={activeRoom===1?styles:{}} className="row">
                     <Button disabled={activeRoom!==1} className={activeRoom>1?'map-button button1 finished':'map-button button1'} callback={()=>{callback('room1')}} value='1' name='Level 1'  />
-                </div>
+                </animated.div>
             </div>
         </div>
     )
