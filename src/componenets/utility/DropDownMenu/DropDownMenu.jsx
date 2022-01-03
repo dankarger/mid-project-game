7e9c00cf35b0f93f280f7e621e931c2dcc0e4aaa
dankarger <karger.dan@gmail.com>
2022-01-03 12:52:48 +0200

    fixing netlify errs

--- src/componenets/utility/DropDownMenu/DropDownMenu.jsx
+++ src/componenets/utility/DropDownMenu/DropDownMenu.jsx
@@ -12,25 +12,23 @@ const DropDownMenu = ({ list, callback, isOpenAnimation })=>{
 
     useEffect(()=>{
         setIAnimation(isOpenAnimation)
-    },[list])
+    },[list,isOpenAnimation])
 
 
     const showList=()=>{
        return list.map(player=>{
            return (
-
-               <div onClick={()=>callback(player)} className='drop-down-player' key={player.id + Date.now()}>
-                   <Link to='/game'>
-                       <div className='drop-down-item'>
-                           <img className='thumbnail' src={player.avatar} alt="thumbnail"/>
-                           <p> {player.name} </p>
-                           <p>{player.score}</p>
-
-                       </div>
-
-                   </Link>
-               </div>
-
+                <div className={isAnimation?'show':'hide'}>
+                   <div onClick={()=>callback(player)} className='drop-down-player' key={player.id + Date.now()}>
+                       <Link to='/game'>
+                           <div className='drop-down-item'>
+                               <img className='thumbnail' src={player.avatar} alt="thumbnail"/>
+                               <p> {player.name} </p>
+                               <p>{player.score}</p>
+                           </div>
+                       </Link>
+                   </div>
+                </div>
            )
        })
     }
