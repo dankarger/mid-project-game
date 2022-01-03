8928eb4507dcb26b89ceb2989f31c5209dea008d
dankarger <karger.dan@gmail.com>
2022-01-03 17:22:44 +0200

    add health animation

--- src/componenets/Navbar/HealthAnimation.js
+++ src/componenets/Navbar/HealthAnimation.js
@@ -1,27 +1,28 @@
 import React, {useEffect, useState} from "react";
 import {useSpring, animated, config} from "react-spring";
 
-export default function HealthAnimation(num) {
+export default function HealthAnimation({num}) {
+    console.log('nun',num)
     // const [flip, set] = useState(false)
     const[currentNumber,setCurrenNumber]=useState(Number)
     const [oldNumber,setOldNumber] = useState(Number)
     const { number } = useSpring({
         reset: true,
         // reverse: flip,
-        from: { number: 122 },
-        to:{number:12},
-        number: 10,
+        from: { number: oldNumber },
+        to:{number:num},
+        number: 100,
         delay: 200,
         config: config.molasses,
         onRest: () =>  setOldNumber(num),
     })
 
-    useEffect(()=>{
-        setCurrenNumber(num)
-        return ()=>{
-            setOldNumber(num)
-        }
-    },[num,oldNumber,currentNumber])
+    // useEffect(()=>{
+    //     setCurrenNumber(num)
+    //     return ()=>{
+    //         setOldNumber(num)
+    //     }
+    // },[num,oldNumber,currentNumber])
 
     return <animated.div>{number.to(n => n.toFixed(0))}</animated.div>
     // return <animated.div>{number.to(n=>n)}</animated.div>
--- src/componenets/Navbar/Navbar.jsx
+++ src/componenets/Navbar/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar =({roomNumber,currentPlayer,currentEnemy})=>{
     return (
         <div className='navbar'>
             <div className=' navbar-item player-info'> <img className='avatar-nav' src={currentPlayer.avatar} alt="avatar-img"/>
-              <div className='navbar-item'>{currentPlayer.name} <span><i className="fas fa-heart"> </i>{currentPlayer.health}</span></div></div>
-            <div> <HealthAnimation num={currentPlayer.health}/></div>
+              <div className='navbar-item'>{currentPlayer.name} <span><i className="fas fa-heart"> </i><HealthAnimation num={currentPlayer.health}/></span></div></div>
+              <div> <HealthAnimation num={currentPlayer.health}/></div>
               <div className='navbar-item navbar-room'>Level: {roomNumber}</div>
               <div className='navbar-item'>{currentEnemy.name} <span><i className="fas fa-heart"> </i>{currentEnemy.health}</span></div>
             <div className='navbar-item'><Button className='settings' name='Settings' /></div>
