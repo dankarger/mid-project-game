ed48c9085310fab38a6d2eb9de032da83f6d07a9
dankarger <karger.dan@gmail.com>
2022-01-03 17:16:39 +0200

    fix netlify errors

--- src/componenets/Navbar/Navbar.jsx
+++ src/componenets/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './Navbar.css'
 import Button from "../utility/Button/Button";
+import HealthAnimation from "./HealthAnimation";
 
 const Navbar =({roomNumber,currentPlayer,currentEnemy})=>{
 
@@ -8,6 +9,7 @@ const Navbar =({roomNumber,currentPlayer,currentEnemy})=>{
         <div className='navbar'>
             <div className=' navbar-item player-info'> <img className='avatar-nav' src={currentPlayer.avatar} alt="avatar-img"/>
               <div className='navbar-item'>{currentPlayer.name} <span><i className="fas fa-heart"> </i>{currentPlayer.health}</span></div></div>
+            <div> <HealthAnimation num={currentPlayer.health}/></div>
               <div className='navbar-item navbar-room'>Level: {roomNumber}</div>
               <div className='navbar-item'>{currentEnemy.name} <span><i className="fas fa-heart"> </i>{currentEnemy.health}</span></div>
             <div className='navbar-item'><Button className='settings' name='Settings' /></div>
